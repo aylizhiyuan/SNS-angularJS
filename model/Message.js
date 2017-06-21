b55c0235b3688350f7582df7512b340647dd5760
lzy <148334733@qq.com>
2017-06-21 18:01:33 +0800

    正在思考二级留言的何去何从啊！！！！

--- model/Message.js
+++ model/Message.js
@@ -9,7 +9,7 @@ const MessageSchema = new Schema({
     /*
      * type:
      * reply: xx 回复了你的话题
-     * reply2: xx 在话题中回复了你
+     * comment: xx 在话题中回复了你
      * follow: xx 关注了你
      * at: xx ＠了你
      */
